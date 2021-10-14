9af7bf2efe7915862797f7c5af3c09caaa64996b
cvelez <cristhian.velez@landers.com.co>
2021-10-14 12:20:41 -0500

    clase 1 fundamentos a javascript

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+
+// Arreglos en JS
+// const arreglo = new Array( 100 );
+const arreglo = ["wilfer","juan pablo","daniel","anderson"];
+console.log(arreglo[3])
+// arreglo.push(1)
+// arreglo.push(2)
+// arreglo.push(3)
+// arreglo.push(4)
+
+//console.log(arreglo)
+
+// let arreglo2 = [ ...arreglo, 5 ];
+
+// const arreglo3 = arreglo2.map( function(numero) {
+//     return numero * 2;
+// });
+
+
+// console.log( arreglo );
+// console.log( arreglo2 );
+// console.log( arreglo3 );
+
+
+
+
+
+
+
+
